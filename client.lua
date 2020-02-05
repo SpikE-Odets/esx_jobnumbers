a2f7e6dbb162d8ffb8da1af09fb3a4188b4ca5af
SpikE-Odets <51069593+SpikE-Odets@users.noreply.github.com>
2020-02-05 05:20:11 -0500

    Update client.lua

--- client.lua
+++ client.lua
@@ -29,22 +29,6 @@ AddEventHandler('esx_jobnumbers:setjobs', function(jobslist)
    JobCount = jobslist
 end)
 
-  -- uncomment this if you want to make sure the script is updating   just use command /jobonline jobname
---[[
-RegisterCommand('jobonline', function(source, args, rawCommand)
-    local jobname = args[1]
-    if args[1] ~= 'all' then
-        print('This is how many '.. tostring(args[1]) ..' are online: '..  tostring(exports["esx_jobnumbers"]:jobonline(jobname)))
-    else
-        print('Staring print List')
-        for i,v in pairs(JobCount) do
-            print('Name: '.. tostring(i).. ' - '.. tostring(v))
-        end
-        print('Ending print List')
-    end
-end)
-]]
-
 function jobonline(joblist)
     for i,v in pairs(Config.MultiNameJobs) do
         for u,c in pairs(v) do
