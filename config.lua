502fd6b01ebb1e98b7baadf294bf50aeba816e77
SpikE-Odets <51069593+SpikE-Odets@users.noreply.github.com>
2022-09-12 03:56:48 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,15 +1,16 @@
 Config = {}
 
--- only use MultiNameJobs for if you need to check total number of both jobs in 1 export - like the example below
+--- Config.JobNames should include any jobs that you will want to have counted... can use
+--- multiple job names in the same as (police/sheriff will count for cops).
 
-Config.MultiNameJobs = {
-   cops = {'police', 'sheriff',},
- -- ems = {'paramadic', 'doctor'},  
+
+Config.JobNames = {
+   ['police'] = 'cops',
+   ['sheriff'] = 'cops',
+   ['ambulance'] = 'medic',
+   ['doctor'] = 'medic',
+   ['mechanic'] = 'mechanic',
 }
 
 
--- example of how to check for police online - using the multiname list above 
---  if  exports["esx_jobnumbers"]:jobonline('cops') > PoliceNeeded then   --  this would return the total number of police/sheriff on the server
 
---  without the multi line would just check as 
---  if  exports["esx_jobnumbers"]:jobonline('police') > PoliceNeeded then   -- this would return only police online if not using multiline.
